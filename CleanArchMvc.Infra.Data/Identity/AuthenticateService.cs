21b31ee98d89b1605d32fd158c0575a25bae03e3
AllanCicceri <allan.cicceri@gmail.com>
2022-09-17 14:38:29 -0300

    add seedUsers and seedRoles

---
+++ CleanArchMvc.Infra.Data/Identity/AuthenticateService.cs
@@ -0,0 +1,40 @@
+using CleanArchMvc.Domain.Account;
+using Microsoft.AspNetCore.Identity;
+
+namespace CleanArchMvc.Infra.Data.Identity
+{
+    public class AuthenticateService : IAuthenticate
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AuthenticateService(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public async Task<bool> Authenticate(string email, string password)
+        {
+            var result = await _signInManager.PasswordSignInAsync(email, password, false, false);
+            return result.Succeeded;
+        }
+
+        public async Task<bool> RegisterUser(string email, string password)
+        {
+            var applicationUser = new ApplicationUser
+            {
+                UserName = email,
+                Email = email 
+            };
+
+            var result = await _userManager.CreateAsync(applicationUser, password);
+            return result.Succeeded;
+        }
+
+        public async Task Logout()
+        {
+            await _signInManager.SignOutAsync();
+        }
+    }
+}
